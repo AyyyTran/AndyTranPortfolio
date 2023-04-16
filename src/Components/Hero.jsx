9fb53be40aab1bb2b9cbfddb4237cf82fdebc056
Andy Tran <AyyyTran@gmail.com>
2023-04-16 14:43:14 -0700

    trying absolute directory

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
           >
             <img
               className="w-5 h-5 fill-white"
-              src="./src/Images/linkedin.svg"
+              src="/src/Images/linkedin.svg"
               alt="LinkedIn"
             />
           </a>

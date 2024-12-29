846a9ecc871b8f9350dc3e47fbc5f1ebeb976962
AyyyTran <AyyyTran@gmail.com>
2024-12-28 17:55:05 -0800

    edit about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -21,37 +21,22 @@ const About = () => {
         </Fade>
         <div className="flex flex-col text-start bg-card p-4 rounded-lg pl-8">
           <p className="text-lg text-primary font-bold py-2 md:text-xl">
-            BCIT Graduate of Computer Systems Technology
+            BCIT Student in Network Security, Bachelor of Applied Science
           </p>
           <p className="text-4xl pb-2 font-bold md:text-5xl">
             Hi! I am Andy Tran.
           </p>
-          <p className="tracking-wide text-md pt-4 ">
-            <b>Who am I?</b> I'm an innovative Full Stack Developer who has a
-            passion for transforming designs and visions into pratical
-            applications that can improve people's lives.
+          <p className="tracking-wide text-md pt-4">
+            <b>Who am I?</b> I'm a developer passionate about turning ideas into practical, impactful solutions.
           </p>
           <p className="tracking-wide text-md pt-4">
-            My journey in the tech world began with an interest in building a
-            computer from scratch. From there I dove deeper into what I can do
-            with computers and learned to love being able to craft a solution to
-            problems. After earning my BCIT Diploma in Computer Systems
-            Technology, I embarked on a path as a Full Stack Developer. From
-            building my first website to collaborating on complex projects with
-            clients, each step has been a rewarding learning experience.
+            My journey in tech began with building computers, which sparked my interest in problem-solving through technology. After earning my diploma in Computer Systems Technology from BCIT, I've worked on a variety of projects, from my first website to collaborating on client solutions. Currently, I’m pursuing a Bachelor of Applied Science in Network Security at BCIT.
           </p>
           <p className="tracking-wide text-md pt-4">
-            As a Full Stack Developer, I have a deep understanding of front-end
-            technologies such as HTML, CSS, JavaScript, React and coupled with a
-            strong command of back-end technologies like C# and .NET along with
-            experience working with databases, RESTful APIs, and cloud platforms
-            helps me to deliver quality and efficient applications
+            I have a strong foundation in technologies such as web development, databases, APIs, cloud platforms, and testing. I’ve gained hands-on experience in different stages of software development and enjoy each aspect of the process.
           </p>
           <p className="tracking-wide text-md pt-4">
-            I find joy coding as I can take ideas from concept to reality and
-            craft applications that have a purpose and will leave a positive
-            impact on people's lives. Aside from coding, I love being active
-            whether it be golf, climbing, or volleyball!
+            I'm now seeking a position where I can continue learning and growing within the tech industry. Let's connect!
           </p>
         </div>
       </div>
